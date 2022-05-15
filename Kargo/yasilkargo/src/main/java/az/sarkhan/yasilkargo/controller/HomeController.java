e752f3c319e235596af43b7736afe85ac80673ed
Serxan-Babayev-7599 <64852621+Serxan-Babayev-7599@users.noreply.github.com>
2022-05-15 13:07:09 +0400

    Code optimization

--- Kargo/yasilkargo/src/main/java/az/sarkhan/yasilkargo/controller/HomeController.java
+++ Kargo/yasilkargo/src/main/java/az/sarkhan/yasilkargo/controller/HomeController.java
@@ -9,4 +9,8 @@ public class HomeController {
     public String getHome(){
         return "home";
     }
+    @GetMapping(path = "/nav")
+    public String getNav(){
+        return "nav";
+    }
 }
